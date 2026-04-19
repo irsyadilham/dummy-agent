d4f6e467760cc986bd766c7c3e7b107c5ba3557d
irsyadilham <irsyadmhdilham@gmail.com>
2026-04-19 13:37:50 +0800

    first commit

---
+++ app/main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+from fastapi.requests import Request
+import json
+import pika
+
+credentials = pika.PlainCredentials(username='admin', password='strongpassword')
+
+app = FastAPI()
+
+@app.get('/test')
+def test():
+    return { 'message': 'Succeed' }
+
+@app.post('/mula-imbasan')
+async def mula_imbasan(request: Request):
+    body = await request.json()
+    tugasan_id = body['tugasan_id']
+
+    with open('scanned_data.json', 'r') as cbom:
+        cbom_data = json.load(cbom)
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            host='178.128.116.198',
+            port=5672,
+            credentials=credentials
+        )
+    )
+
+    channel = connection.channel()
+
+    channel.queue_declare(queue='tugasan_queue')
+
+    data = {
+        'tugasan_id': tugasan_id,
+        'cbom_data': cbom_data
+    }
+
+    channel.basic_publish(
+        exchange='',
+        routing_key='tugasan_queue',
+        body=json.dumps(data)
+    )
+
+    connection.close()
+    
+    return data
